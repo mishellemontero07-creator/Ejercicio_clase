b6e2421645dfca29dcbe5c4deb8f8a0a928ca4d3
Laboratorio <laboratorio@udla-ec.int>
2026-04-02 15:21:39 -0500

    primer commit- ejercicio_clase

---
+++ ejericicio_clase.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+ 
+int main() {
+    int n, i;
+    int A[50];
+
+ 
+    // Leer el número de elementos
+    printf("Ingrese el numero de elementos: ");
+    scanf("%d", &n);
+    
+    if(n<0){
+     printf("el numero ingresado no es positivo!! ");
+     return 0;
+    }
+ 
+    // Leer los elementos del vector
+    for(i = 0; i < n; i++) {
+        printf("Ingrese el elemento %d: ", i + 1);
+        scanf("%d", &A[i]);
+    }
+ 
+    // Imprimir los elementos del vector
+    for(i = 0; i < n; i++) {
+        printf("%d\n", A[i]);
+    }
+ 
+    return 0;
+}
