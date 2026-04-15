7f508c9a3a0c2a192beedfeda57d44604ad58cb0
Gabriela Montero <mishellemontero07@gmail.com>
2026-04-15 01:35:48 -0500

    bloque 4 y 5 terminados

--- Tarea2.c
+++ Tarea2.c
@@ -4,6 +4,8 @@ int main(int argc, char const *argv[])
     int notas[5][3];
     int nota, suma;
     float promedio;
+    int max, min;
+
     
 
     //bloque 1: ingreso de datos y validación de nota
@@ -53,7 +55,57 @@ int main(int argc, char const *argv[])
         }
 
         promedio = suma/5.0;
-        printf("El promedio de la asignatura %d es de %.2f\n ", i+1, promedio); 
+        printf("El promedio de la asignatura %d es de %.2f\n ", j+1, promedio); 
+    }
+
+    //bloque 4: nota más alta y baja por asignatura
+
+     for (int j=0; j < 3; j++)
+    {
+        max= notas[0][j]; 
+        min= notas[0][j]; 
+    
+        for(int i=0; i < 5; i++)
+        { 
+        
+        if (notas[i][j] > max)
+            {
+             max = notas[i][j];
+            } 
+
+        if (notas [i][j] < min)
+        {
+            min= notas [i][j];
+        }
+        
+        printf("Asignatura %d: máxima = %d, mínima = %d\n", j+1, max, min);
+
+    }
+
+
+    //bloque 5:  nota más alta y baja por estudiante 
+
+
+     for (int i=0; i < 5; i++)
+    {
+
+        max= notas[i][0]; 
+        min= notas[i][0]; 
+
+        for(int j=0; j < 3; j++)
+        { 
+         if (notas[i][j] > max)
+            {
+             max = notas[i][j];
+            } 
+
+        if (notas [i][j] < min)
+            {
+            min= notas [i][j];
+            }
+        }
+
+     printf("Estudiante %d: máxima = %d, mínima = %d\n", i+1, max, min);
     }
     
     return 0;
