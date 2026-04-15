d341ed4f2fd9f53e09dd0b25a1b4f53717fc2791
Gabriela Montero <mishellemontero07@gmail.com>
2026-04-15 00:28:37 -0500

    fin del bloque 1-validación de datos

--- Tarea2.c
+++ Tarea2.c
@@ -2,17 +2,33 @@
 int main(int argc, char const *argv[])
 {
     int notas[5][3];
+    int nota, suma;
+    float promedio;
+    
 
-
+    //bloque 1: ingreso de datos y validación de nota
     for (int i=0; i < 5; i++)
     {
         for(int j=0; j < 3; j++)
         {
 
-            printf("Ingresa la nota del estudiante %d para asignatura %d", i++, j++ );
-            scanf("%d", &notas[i][j]);
+            printf("Ingresa la nota del estudiante %d para asignatura %d: ", i+1, j+1);
+            scanf("%d", &nota);
+
+            while (nota < 0 || nota >10)
+            {
+               
+                printf("ERROR!!, nota ingresada inválida, intentelo de nuevo\n");
+                scanf("%d", &nota);
+
+            }
+            notas[i][j]= nota;  
         }
     }
 
+    
+
+
     return 0;
 }
+
