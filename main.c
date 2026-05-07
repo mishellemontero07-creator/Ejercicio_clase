81c28192813e4c55d309d59f5fda665343818712
Gabriela Montero <gabriela.montero@udla.edu.ec>
2026-05-07 14:47:45 -0500

    quinto commit: multiplicar y dividir

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include "sumar.h"
+#include "restar.h"
+#include "multiplicar.h"
+#include "dividir.h"
+
 
 
 int sumar (int a, int b);
@@ -11,8 +15,11 @@ int main (int argc, char const*argv [])
 
  printf ("ingrese 2do número: ");
  scanf("%d", &b);
+     printf("El resultado de la suma es: %d", sumar(a,b));
+    printf("El resultado de la resta es:%d", restar(a,b));
+    printf("El resultado de la multiplicación es: %d ", multiplicar(a,b));
+    printf("El resultado de la división es: %d ", dividir(a,b));
 
- printf("%d", suma(a,b));
  return 0;
 
 }
