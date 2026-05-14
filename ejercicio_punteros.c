c218ec88e95d0dc94f339a44084d71b0ede5b0b4
Laboratorio <laboratorio@udla-ec.int>
2026-05-14 13:42:52 -0500

    commit subir al repo github

---
+++ ejercicio_punteros.c
@@ -0,0 +1,38 @@
+// Fecha: 11/05/2026
+//Ejercicio clase Punteros
+
+#include <stdio.h>
+int suma(int a,int b);
+int sumaxref(int *a, int *b);
+
+
+int main(){
+
+    int a, b;
+
+    printf("Ingrese el valor: \n");
+    scanf("%d", &a);
+
+    printf("Ingrese el valor de b: \n");
+    scanf("%d", &b);
+
+    int resultado= suma(a,b);
+
+    printf("El resultado de la suma es: %d\n", suma(a,b));
+    
+    //Aqui estamos imprimiendo los punteros, pero OJO cómo hacemos que lea la variable "sumaref", con &. 
+    printf("El resultado de la suma es: %d\n", sumaxref(&a,&b));
+
+}
+
+//Aqui solo es una declaración de funció suma simple
+int suma(int a,int b){
+    return a+b;
+
+}
+
+//Aqui estamos usando punteros
+int sumaxref(int *a,int *b){
+    return *a + *b;
+    
+}
