d99cfc5cc05fd6b3405cc89933a25c89c1beb901
Laboratorio <laboratorio@udla-ec.int>
2026-05-14 13:56:18 -0500

    codigo del profe

--- ejercicio_punteros.c
+++ ejercicio_punteros.c
@@ -36,3 +36,31 @@ int sumaxref(int *a,int *b){
     return *a + *b;
     
 }
+
+
+/*Código del profe
+
+#include <stdio.h>
+int suma(int a,int b);
+int sumaxref(int *a,int *b);
+
+ 
+
+int main() {
+    int a,b;
+    printf("valor de a:");
+    scanf("%d", &a);
+    printf("valor de b:");
+    scanf("%d", &b);
+    printf("La respuesta de la suma es: %d\n", suma(a,b));
+    printf("La respuesta de la suma es: %d\n", sumaxref(&a,&b));
+}
+int suma(int a,int b){
+        return a+b;
+}
+int sumaxref(int *a,int *b){
+        return *a + *b;
+}
+
+
+*/
